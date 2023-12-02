b743dfcd28aa63f6481b895e775e93f8f9903395
munjinpark <58156442+munjinpark@users.noreply.github.com>
2023-12-03 07:50:07 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # from dotenv import load_dotenv
 # load_dotenv()
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from lcProcess import LangChainProcess
 
